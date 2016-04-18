564c1d9ca0d8ef04ab4123718cfd565cb6b8c1e4
omainada <omainada@cisco.com>
2016-04-18 22:34:15 +0200

    first commit stable version

---
+++ config.php
@@ -0,0 +1,29 @@
+<?php
+	$dbServer = '127.0.0.1:3306';
+	$dbUsername = 'root';
+	$dbPassword = 'D!M!jean12';
+	$dbDatabase = 'DEPENSES_DB';
+
+	$adminConfig = array(
+		'adminUsername' => "omainada",
+		'adminPassword' => "c0f9048c5c0d37555305eb439f5b4dc0",
+		'notifyAdminNewMembers' => "",
+		'defaultSignUp' => "1",
+		'anonymousGroup' => "anonymous",
+		'anonymousMember' => "guest",
+		'groupsPerPage' => "10",
+		'membersPerPage' => "10",
+		'recordsPerPage' => "10",
+		'custom1' => "Full Name",
+		'custom2' => "Address",
+		'custom3' => "City",
+		'custom4' => "State",
+		'MySQLDateFormat' => "%m/%d/%Y",
+		'PHPDateFormat' => "n/j/Y",
+		'PHPDateTimeFormat' => "m/d/Y, h:i a",
+		'senderName' => "Membership management",
+		'senderEmail' => "omainada@cisco.com",
+		'approvalSubject' => "Your membership is now approved",
+		'approvalMessage' => "Dear member,\n\nYour membership is now approved by the admin. You can log in to your account here:\nhttp://localhost/orpm\n\nRegards,\nAdmin",
+		'hide_twitter_feed' => ""
+	);
